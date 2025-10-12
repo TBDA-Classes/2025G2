3a08e521fd22e23a3ed541b2d067342916ed5bd7
Atle <atle.sund@alumnos.upm.es>
2025-10-12 17:39:49 +0200

    final backend setup

--- backend/database.py
+++ backend/database.py
@@ -15,6 +15,5 @@ db_host = os.getenv("DB_HOST")
 db_name = os.getenv("DB_NAME")
 db_port = os.getenv("DB_PORT")
 
-# Using the default port 5432
 DATABASE_URL = f'postgresql+psycopg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 engine = create_engine(DATABASE_URL)
