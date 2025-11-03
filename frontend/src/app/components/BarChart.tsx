04168561b0e8202f25ccaa7d2fce742d3e12bf52
Atle <atle.sund@alumnos.upm.es>
2025-11-03 12:03:55 +0100

    helped on discovering a small bug

--- frontend/src/app/components/BarChart.tsx
+++ frontend/src/app/components/BarChart.tsx
@@ -1,4 +1,3 @@
-// 'use client' // This is necessary for the responsive component to work
 'use client'
 import { DateState } from '@/types/DateState';
 import { ResponsiveBar } from '@nivo/bar'
@@ -6,6 +5,7 @@ import { ResponsiveBar } from '@nivo/bar'
   const BarChart = ({ _data }: { _data: DateState }) => {
     const chartData = [
       {
+        day: _data.date,  // Use the date as the index
         idle: _data.state_idle,
         active: _data.state_active,
         running: _data.state_running
@@ -18,20 +18,44 @@ import { ResponsiveBar } from '@nivo/bar'
       <ResponsiveBar
           data={chartData}
           keys={keys}
-          indexBy="state"
-          margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
-          padding={0.15}
+          indexBy="day"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.3}
           layout="vertical"
           colors={{ scheme: 'red_blue' }}
           borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-          axisTop={{}}
-          axisBottom={{ legend: 'day', legendOffset: 36 }}
-          axisLeft={{ legend: 'hours of use', legendOffset: -40 }}
-          enableGridX={true}
-          enableGridY={false}
+          axisTop={null}  
+          axisBottom={{ 
+            legend: 'Date', 
+            legendOffset: 36,
+            legendPosition: 'middle'
+          }}
+          axisLeft={{ 
+            legend: 'Hours of Use', 
+            legendOffset: -40,
+            legendPosition: 'middle'
+          }}
+          enableGridX={false}
+          enableGridY={true}
           labelSkipWidth={12}
           labelSkipHeight={12}
           labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+          legends={[
+            {
+              dataFrom: 'keys',
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: 'left-to-right',
+              itemOpacity: 0.85,
+              symbolSize: 20,
+            }
+          ]}
       />
     );
   };
