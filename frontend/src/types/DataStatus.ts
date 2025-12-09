318d69697e86f21e9453cc0884db9130d2475922
jovanbajcetic <jovan.bajcetic@alumnos.upm.es>
2025-12-09 15:36:44 +0100

    Delete frontend directory

--- frontend/src/types/DataStatus.ts
+++
@@ -1,7 +0,0 @@
-export interface DataStatus{
-    table_name: string;
-    first_date: string;
-    last_date: string;
-    total_records: number;
-    number_of_sensors: number;
-}
--- frontend/src/types/DateState.ts
+++
@@ -1,6 +0,0 @@
-export interface DateState{
-    date: string;
-    state_idle: number;
-    state_active: number;
-    state_running: number;
-}
